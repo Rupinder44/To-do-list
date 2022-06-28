6cf539d306364ec7e71f77add5087a8e528e91c6
Rupinder <rupinderkaur8804@gmail.com>
2022-06-28 14:30:11 +0530

    made a to do list without backend

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+const itemsSchema = {
+    name: String
+};
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to ur todo list"
+});
+const item2 = new Item({
+    name: "Hit the + button to add a new item"
+});
+const item3 = new Item({
+    name: "Hit the - button to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
+app.get("/", (req, res) => {
+    Item.find((err, items) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (items.length === 0) {
+                Item.insertMany(defaultItems, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("Successfully added many items");
+                    }
+                });
+                res.redirect("/");
+            }
+            res.render("list", { kindOfDay: "Today", newListItems: items })
+        }
+    })
+    // res.render("list", { kindOfDay: "Today", newListItems: items })
+});
+app.get("/work", (req, res) => {
+    var title = "Today's work"
+    res.render("list", { kindOfDay: title, newListItems: workItems })
+})
+app.post("/", (req, res) => {
+    var item = req.body.newItem;
+    if (req.body.list === "Today's") {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
+})
+app.post("/work", (req, res) => {
+    var wItem = req.body.newItem;
+    workItems.push(wItem);
+    res.redirect("/work");
+})
+
+app.listen(3000, () => {
+    console.log("server is up and running")
+});
+
+
+
+
+
+
+
+
+
+// const express=require("express");
+// const app=express();
+// const bodyParser=require("body-parser");
+
+// app.set("view engine","ejs")
+
+// app.get("/",(req,res)=>{
+//     var currD = new Date().getDay();
+//     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//     var day = days[currD];
+//     // if(currD===6||currD===0){
+//     //     day = "Weekend";
+//     // }
+//     // else{
+//     //     day = "Weekday";
+//     // }
+//     res.render("list",{kindOfDay: day})
+// });
+
+// app.listen(3000,()=>{
+//     console.log("server is up and running")
+// });
